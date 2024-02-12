5734decfb42bad85fb09f095b0c8151a2046bd24
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-12 19:03:50 +0300

    modified

--- Passenger Counter/script.js
+++ Passenger Counter/script.js
@@ -4,6 +4,23 @@
 
 // console.log(count)
 
-function increment() {
-    console.log("the button was clicked")
-}
+// function increment() {
+//     console.log("the button was clicked")
+// }
+
+// // UNDERSTANDING FUNCTIONS
+// function countDown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countDown()
+
+// function first() {
+//     console.log(42)
+// }
+
+// first()
