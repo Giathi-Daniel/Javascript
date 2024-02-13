e12225436e010209883adb75915f09bba228994c
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-13 12:27:53 +0300

    updated

--- Passenger Counter/script.js
+++ Passenger Counter/script.js
@@ -1,27 +1,17 @@
-// document.getElementById("count-el").innerText = 5
-
-// let count = 0
-
-// console.log(count)
-
-// function increment() {
-//     console.log("the button was clicked")
-// }
-
-// // UNDERSTANDING FUNCTIONS
+// UNDERSTANDING FUNCTIONS
 let saveEl = document.getElementById("save-el")
 let countEL = document.getElementById("count-el");
 let count = 0
 
 function increment() {
     count += 1
-    countEL.innerText = count
+    countEL.textContent = count
 }
 
 function save() {
-    let countStr = count + " - " //a variable that contains both the dashes and the separator
-    saveEl.textContent += countStr //rendering the variable in the saveEl using innerTex
-    // console.log(count)
+    let countStr = count + " - " 
+    saveEl.textContent += countStr 
+    countEL.textContent = 0
+    count = 0
 }
 
-save()
