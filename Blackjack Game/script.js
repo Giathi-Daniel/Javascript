39073f0e9de4e15c8b776fd6d95d05a17f53269f
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-14 11:35:07 +0300

    BlackJack

--- Blackjack Game/script.js
+++ Blackjack Game/script.js
@@ -1,25 +1,26 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard + 20
-let hasBlackJack = false 
-let message = ""
-let isAlive = true
-// let messageEl = document.getElementById("message-el")
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard + 6;
+let hasBlackJack = false;
+let message = "";
+let isAlive = true;
+
+let messageEl = document.getElementById("message-el");
+// let cardsEl = document.getElementById("cards-el");
+// let sumEl = document.getElementById("sum-el");
 
 function startGame() {
-    // messageEl.textContent
+    // sum += 20; // Adding 20 to the sum for demonstration purposes
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
-        hasBlackJack = true
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
     } else {
-        message = "Whooops! You loose! 😭"
-        isAlive = false
-    } 
+        message = "Whoops! You lose!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+    // cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard; // Display cards
+    // sumEl.textContent = "Sum: " + sum; // Display sum
 }
-
-
-// CASH OUT
-alert(message)
-alert(hasBlackJack)
