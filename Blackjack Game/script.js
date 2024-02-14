5cc29493ad543875091ef66e76b1150d3d234702
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-14 11:21:52 +0300

    BlackJack

--- Blackjack Game/script.js
+++ Blackjack Game/script.js
@@ -4,16 +4,21 @@ let sum = firstCard + secondCard + 20
 let hasBlackJack = false 
 let message = ""
 let isAlive = true
+// let messageEl = document.getElementById("message-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "Whooops! You loose! 😭"
-    isAlive = false
-} 
+function startGame() {
+    // messageEl.textContent
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "Whooops! You loose! 😭"
+        isAlive = false
+    } 
+}
+
 
 // CASH OUT
 alert(message)
