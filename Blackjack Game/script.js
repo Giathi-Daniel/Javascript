0650f0428d6ffbe894440673e415efd40f55535a
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-14 10:48:55 +0300

    Blackjack

--- Blackjack Game/script.js
+++ Blackjack Game/script.js
@@ -1,11 +1,16 @@
 let firstCard = 6
 let secondCard = 9
-let sum = firstCard + secondCard + 6 + 6
+let sum = firstCard + secondCard + 6
+let hasBlackJack = false 
 
-if (sum < 21) {
+if (sum <= 20) {
     alert("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     alert("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     alert("Whooops! You loose! 😭")
 } 
+
+// CASH OUT
+alert(hasBlackJack)
