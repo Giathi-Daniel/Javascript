f52ac8e3e95c92206ac0d68040d6e49d608dddf2
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-15 16:15:17 +0300

    BlackJack

--- Blackjack Game/script.js
+++ Blackjack Game/script.js
@@ -1,6 +1,7 @@
 let firstCard = 10;
-let secondCard = 10;
-let sum = firstCard + secondCard + 6;
+let secondCard = 1;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let message = "";
 let isAlive = true;
@@ -10,8 +11,30 @@ let cardsEl = document.getElementById("cards-el");
 let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
     sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "Whoops! You lose!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    alert("Drawing a new card from the deck!");
+    let card = Math.floor(Math.random() * 13) + 1; // Generate a random card value between 1 and 13
+    sum += card; // Add the new card value to the sum
+
+    // Update the UI with the new card and sum values
+    cardsEl.textContent += ", " + card;
+    sumEl.textContent = "Sum: " + sum;
+
+    // Check if the player is still alive or has blackjack
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
