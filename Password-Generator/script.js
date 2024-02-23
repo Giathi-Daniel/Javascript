26d5e13200f0dbbb56b3073e311e29b8c330f330
Giathi Daniel <giathidaniel252@gmail.com>
2024-02-23 12:37:31 +0300

    Completed

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,12 +1,28 @@
+// Variables to use
 const passwordLength = 14;
 const lowerCase = true;
 const upperCase = true;
 const includeNumbers = true;
 const includeSymbols = true;
 
+// generating password
 function generatePassword(length, lowerCase, upperCase, includeNumbers, includeSymbols) {
     
-    
+    const lowercase = "abcdefghijklmnopqrstuvwxyz"
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numbers = "0123456789"
+    const symbols = "#$%^&<>?*|@_!`~"
+
+    let allowedChars = ""
+    let password = ""
+
+    allowedChars += lowerCase ? lowercase : "";
+    allowedChars += upperCase ? uppercase : "";
+    allowedChars += includeNumbers ? numbers : "";
+    allowedChars += includeSymbols ? symbols: "";
+
+
+    alert(allowedChars)
     return '';
 }
 
