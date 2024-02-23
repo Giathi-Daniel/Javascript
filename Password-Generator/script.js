42d2ed1fb07f53490f93207222fba1218c6598c7
Giathi Daniel <giathidaniel252@gmail.com>
2024-02-23 12:20:49 +0300

    Password Generator

---
+++ Password-Generator/script.js
@@ -0,0 +1,14 @@
+const passwordLength = 14;
+const lowerCase = true;
+const upperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+function generatePassword(length, lowerCase, upperCase, includeNumbers, includeSymbols) {
+    
+    
+    return '';
+}
+
+const password = generatePassword(passwordLength, lowerCase, upperCase, includeNumbers, includeSymbols);
+console.log(`Password: ${password}`);
