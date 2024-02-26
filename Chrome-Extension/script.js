454a681b8b63b11dc3955ba9ab67e9d47b53e9ea
Giathi Daniel <giathidaniel252@gmail.com>
2024-02-26 13:12:14 +0300

    Updated

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -1,7 +1,12 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById('input-el');
 const saveBtn = document.getElementById('input-btn');
 
 saveBtn.addEventListener("click", function(){
-    console.log('saveBtn clicked');
+    // myLeads.push(inputEl.value);
+    // console.log('myLeads', myLeads);
+
+    for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i])
+    }
 })
