74ca1ad473b404367bbac064c9d60bfc93500d7a
Giathi Daniel <giathidaniel252@gmail.com>
2024-02-26 13:02:16 +0300

    basic Javscript

---
+++ Chrome-Extension/script.js
@@ -0,0 +1,7 @@
+let myLeads = []
+const inputEl = document.getElementById('input-el');
+const saveBtn = document.getElementById('input-btn');
+
+saveBtn.addEventListener("click", function(){
+    console.log('saveBtn clicked');
+})
