128e0bfdea565e67b0a3374211843947bec6b6c0
Daniel-Giathi <giathidaniel252@gmail.com>
2024-02-28 17:22:41 +0300

    Updates

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,59 +1,39 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const storage = JSON.parse(localStorage.getItem("myLeads"));
-alert(storage)
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-if (storage) {
-    myLeads = storage;
-    renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-//clear localStorage when double-clicked!
-deleteBtn.addEventListener("dblclick", () => {
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    // saving the myLeads array to localStorage
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
-}
-
-
-
-
-
-// // WORKING WITH JSON
-// let myLeadS = `["www.w3schools.com"]`
-
-// // converting string to an object
-// myLeadS = JSON.parse(myLeadS);
-// alert(typeof myLeadS)
-
-// // addding a new element to the object
-// myLeadS.push("www.google.com")
-
-// // convertin back object to a string
-// myLeadS  = JSON.stringify(myLeadS)
-// alert(typeof myLeadS)
