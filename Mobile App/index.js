3d1212414801f73b8aa291f7298f9cd2ae78870e
Daniel-Giathi <giathidaniel252@gmail.com>
2024-03-20 13:40:48 +0300

    firebaseRealTimeDatabase

--- Mobile App/index.js
+++ Mobile App/index.js
@@ -1,11 +1,12 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const appSettings = {
+    databaseUrl: "https://mobileapp-f4cf9-default-rtdb.firebaseio.com/"
+}
 const output = document.getElementById("input-field");
 const btnOutput = document.getElementById("add-button");
 
 btnOutput.addEventListener('click', ()=> {
     let inputValue = output.value;
-    console.log(inputValue)
+    console.log(`${inputValue} added to database`)
 })
