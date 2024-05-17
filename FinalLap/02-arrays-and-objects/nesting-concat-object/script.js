55ed1ffee0ed4f89a7e7ca05c8a9ffc6d4475005
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-17 10:25:20 +0300

    refactor

--- FinalLap/02-arrays-and-objects/nesting-concat-object/script.js
+++ FinalLap/02-arrays-and-objects/nesting-concat-object/script.js
@@ -37,7 +37,7 @@ console.log(Arr.reverse())
 const arr1 = [1,2,3,4,5];
 const arr2 = [5,6,7,8,9,10]
 
-// arr1.concat(arr2).splice(arr1.length -1)
+// let arr3 = arr1.slice(0, 4).concat(arr2);
 arr1.pop(4)
 // console.log(arr1)
 let arr3 = [...arr1, ...arr2]
