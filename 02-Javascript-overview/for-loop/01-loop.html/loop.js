e0157aecb1de74bb159dffd45303ccc393af4b3e
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-19 16:14:41 +0300

    add loop-arrays

--- 02-Javascript-overview/for-loop/01-loop.html/loop.js
+++ 02-Javascript-overview/for-loop/01-loop.html/loop.js
@@ -6,4 +6,16 @@ const todoList = [
 
 todoList.forEach(item => {
     console.log(item)
+    console.log(item.length)
+    console.log(todoList.length)
 });
+
+
+// add items -> using accumulator pattern
+const nums = [1, 2, 3]
+let total = 0 // -> accumulator varibale
+
+nums.forEach(num => {
+    total += num
+    console.log(total) 
+})
