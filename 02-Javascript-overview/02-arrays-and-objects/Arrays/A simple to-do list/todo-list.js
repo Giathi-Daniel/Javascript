722459b6a8863d6b7f792c6324abe842ebd262d9
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-19 16:45:09 +0300

    add todo-list

--- 02-Javascript-overview/02-arrays-and-objects/Arrays/A simple to-do list/todo-list.js
+++ 02-Javascript-overview/02-arrays-and-objects/Arrays/A simple to-do list/todo-list.js
@@ -11,4 +11,18 @@ addBtn.addEventListener('click', () => {
 
     // refresh when the btn is clicked
     todoInput.value = ""
+
+    // => Challenge (complete the code below to render in HTML doc as well as show when an element is being added to the array)
+    todoList.forEach(list => {
+        const listItem =  `<p>${list}</p>`;
+
+        document.querySelector('.js-todo-list-simple-2').innerHTML = listItem;
+
+        console.log(listItem)
+
+            // method b
+        // let todoListHTML = ''
+        // todoListHTML += listItem
+        // document.querySelector('.js-todo-list-simple-2').innerHTML = todoListHTML;
+    })
 })
