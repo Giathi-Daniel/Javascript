0819e0f98ef318eef964aa99c9c0be2615e40a61
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-22 05:48:27 +0300

    save cart in localStorage

--- 03-Beginner-projects/Amazon-add-to-cart/data/cart.js
+++ 03-Beginner-projects/Amazon-add-to-cart/data/cart.js
@@ -1,13 +1,23 @@
-export let cart = [
-    {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
-    }
-];
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) {
+    //changing it from giving a null rather to a default value
+    cart = [
+        {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2
+        },
+        {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1
+        }
+    ];
+}
+
 
 // function for addtocart
 export function addToCart(productId) {
@@ -26,6 +36,8 @@ export function addToCart(productId) {
             productId: productId,
             quantity: 1
         }); 
+
+        saveToStorage();
   }
 
   export function removeFromCart(productId) {
@@ -38,4 +50,6 @@ export function addToCart(productId) {
     })
 
     cart = newCart
+
+    saveToStorage();
   }
