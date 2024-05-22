18ac1448df63dfd28585324229300b1198c4cd9d
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-22 07:43:10 +0300

    Add listItem dom

--- 03-Beginner-projects/shopping-list/script.js
+++ 03-Beginner-projects/shopping-list/script.js
@@ -8,3 +8,24 @@ const title = document.getElementById('app-title')
 title.innerHTML = '<strong>Shopping List</strong>'
 
 title.style.color = "#fe32e"
+
+// QueryseclectorAll()
+const listItems = document.querySelectorAll('.item')
+
+// listItems.forEach((item, index) => {
+//     item.style.color = "violet-blue"
+
+//     if(index === 1) {
+//         item.remove()
+//     }
+
+//     if(index === 4) {
+//         item.innerHTML = ` Sweetmelon
+//         <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//         </button>`
+//     }
+// })
+
+const listItemsClasses = document.getElementsByClassName('item');
+console.log(listItemsClasses[1].innnerText);
