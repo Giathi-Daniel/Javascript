9f587a1068d140b9b727abdd1fe706c80d8d015c
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-26 13:53:44 +0300

    Rename tetst-jasmine to tests

--- 03-Beginner-projects/Amazon-add-to-cart/tests-jasmine/tests-simple/moneyTest.js
+++ 03-Beginner-projects/Amazon-add-to-cart/tests-jasmine/tests-simple/moneyTest.js
@@ -1,5 +1,5 @@
 // testing code -> automated test
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 console.log('test suite: formatCurrency') //group of tests named formatCurrency
 
