29274fd6970824b6a59730c9d6e3ad0f6972d179
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-31 14:28:27 +0300

    Add create elements

--- 02-Javascript-overview/05-document-object-model/04-create-elements/script.js
+++ 02-Javascript-overview/05-document-object-model/04-create-elements/script.js
@@ -9,3 +9,37 @@ div.appendChild(text)
 document.body.appendChild(div) //very end of the body tag
 
 // console.log(div)
+
+// Create a function to add a list in the HTML document
+// 1. Quick way and dirty
+function createListItem(item) {
+    const li = document.createElement('li')
+
+    li.innerHTML = `
+        ${item}
+        <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"><i>
+        </button>
+    `
+
+    document.querySelector('.items').appendChild(li)
+}
+createListItem('Eggs')
+
+// 2. Clean & Perfomant -> creat each element
+function createNewItem(item) {
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(item))
+
+    const button = document.createElement('button')
+    button.className = 'remove-item btn-link text-red'
+
+    const icon = document.createElement('i')
+    icon.className = 'fa-solid fa-xmark'
+
+    button.appendChild(icon)
+    li.appendChild(button)
+
+    document.querySelector('.items').appendChild(li)
+}
+createNewItem('Onions')
